df78ed5a79ebd14b82dfb27dad110a091869b19c
Ayan Bhattacharya <67321022+anefreeinity@users.noreply.github.com>
2024-01-29 00:48:30 +0530

    n array tree deletion

--- NArrayTree/main.cpp
+++ NArrayTree/main.cpp
@@ -4,12 +4,19 @@ int main()
 {
     NArrayTree parent("lev 1");
 
-    NArrayTree *child1 = parent.Insert("lev 2.1");
-    child1->Insert("lev 3.2.1");
-    child1->Insert("lev 3.2.2");
+    NArrayTree *child1 = parent.Insert("lev 1-2.1");
+    child1->Insert("lev 1-2.1-3.1");
+    child1->Insert("lev 1-2.1-3.2");
 
-    parent.Insert("lev 2.2")->Insert("lev 3.2.2");
-    parent.Insert("lev 2.3");
+    parent.Insert("lev 1-2.2")->Insert("lev 1-2.2-3.1");
+    parent.Insert("lev 1-2.3");
 
     parent.Display();
+    std::cout << std::endl;
+
+    parent.Delete("lev 1-2.1");
+    std::cout << std::endl;
+
+    parent.Display();
+    std::cout << std::endl;
 }
--- NArrayTree/n_array_tree.cpp
+++ NArrayTree/n_array_tree.cpp
@@ -8,17 +8,20 @@ public:
     std::string message;
     std::vector<NArrayTree *> children;
     int levCount;
+    bool isDeleted;
 
     NArrayTree(std::string message)
     {
         this->message = message;
         levCount = 0;
+        isDeleted = false;
     }
 
     NArrayTree(std::string message, int levCount)
     {
         this->message = message;
         this->levCount = levCount;
+        isDeleted = false;
     }
 
     NArrayTree *Insert(std::string message)
@@ -40,5 +43,43 @@ public:
         {
             child->Display();
         }
+    }
+
+    void Delete(std::string message)
+    {
+        if (this->message == message)
+        {
+            DeleteAllChildren(this);
+        }
+
+        int pos = -1;
+        for (int i = 0; i < children.size(); i++)
+        {
+            if (children[i]->message == message)
+            {
+                pos = i;
+            }
+            children[i]->Delete(message);
+            if (pos != -1 && pos < children.size() && children[pos]->message == message)
+            {
+                delete children[pos];
+                children.erase(children.begin() + pos);
+            }
+        }
+    }
+
+    void DeleteAllChildren(NArrayTree *node)
+    {
+        for (NArrayTree *child : node->children)
+        {
+            DeleteAllChildren(child);
+            delete child;
+        }
+        children.clear();
+    }
+
+    ~NArrayTree()
+    {
+        std::cout << "deleted: " << message << std::endl;
     }
 };
