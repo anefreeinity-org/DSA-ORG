d77eec6aac693668e47670b47dad1c87adc8be7c
ankan5960 <ankanmaity27@gmail.com>
2024-03-06 21:25:55 +0530

    min_heap_add

---
+++ Heap/binary_min_heap.cpp
@@ -0,0 +1,113 @@
+#include <vector>
+#include <iostream>
+
+class BinaryMaxHeapMin
+{
+public:
+    std::vector<int> heapArr;
+    int length;
+
+    BinaryMaxHeapMin()
+    {
+        length = 0;
+    }
+
+private:
+    int FindLeftChildIndex(int parentIndx)
+    {
+        return (2 * parentIndx + 1);
+    }
+
+    int FindRightChildIndex(int parentIndx)
+    {
+        return (2 * parentIndx + 2);
+    }
+
+    int FindParentIndex(int childIndx)
+    {
+        return (childIndx - 1) / 2;
+    }
+
+    int Swap(int first, int second, bool isDeleting = true)
+    {
+        if (isDeleting)
+        {
+            if (first > length - 1 || second > length - 1)
+                return 0;
+        }
+        int temp = heapArr[first];
+        heapArr[first] = heapArr[second];
+        heapArr[second] = temp;
+        return 1;
+    }
+
+    void MaintainHeigherArchyAfterInsertation(int index)
+    {
+        if (index == 0)
+        {
+            return;
+        }
+
+        int currentParentIndex = FindParentIndex(index);
+
+        if (heapArr[currentParentIndex] > heapArr[index])
+        {
+            Swap(index, currentParentIndex, false);
+            MaintainHeigherArchyAfterInsertation(currentParentIndex);
+        }
+    }
+
+    void MaintainHeigherArchyAfterDeletion(int index)
+    {
+        if (index >= length - 1)
+        {
+            return;
+        }
+
+        int leftChildIndex = FindLeftChildIndex(index);
+        int rightChildIndex = FindRightChildIndex(index);
+
+        if (heapArr[index] > heapArr[leftChildIndex] && heapArr[leftChildIndex] < heapArr[rightChildIndex])
+        {
+            if (Swap(index, leftChildIndex))
+                MaintainHeigherArchyAfterDeletion(leftChildIndex);
+        }
+        else if (heapArr[index] > heapArr[leftChildIndex] && heapArr[leftChildIndex] > heapArr[rightChildIndex])
+        {
+            if (Swap(index, rightChildIndex))
+                MaintainHeigherArchyAfterDeletion(rightChildIndex);
+        }
+    }
+
+public:
+    void Insert(int data)
+    {
+        heapArr.push_back(data);
+        length++;
+        MaintainHeigherArchyAfterInsertation(length - 1);
+    }
+
+    int Delete()
+    {
+        if (length == 0)
+        {
+            return -1;
+        }
+
+        int temp = heapArr[0];
+        Swap(0, length - 1);
+        heapArr.pop_back();
+        length -= 1;
+        MaintainHeigherArchyAfterDeletion(0);
+        return temp;
+    }
+
+    int FindxMin()
+    {
+        if (length == 0)
+        {
+            return -1;
+        }
+        return heapArr[0];
+    }
+};
--- Heap/main.cpp
+++ Heap/main.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 #include "binary_max_heap.cpp"
+#include "binary_min_heap.cpp"
 
 int main()
 {
-    BinaryMaxHeap heap;
+    BinaryMaxHeapMin heap;
     heap.Insert(1);
     heap.Insert(0);
     heap.Insert(8);
@@ -12,15 +13,30 @@ int main()
     heap.Insert(4);
     heap.Insert(6);
 
-    std::cout << heap.Delete() << std::endl
-              << std::endl;
+    // std::cout << heap.Delete() << std::endl
+    //           << std::endl;
 
-    std::cout << heap.Delete() << std::endl
-              << std::endl;
+    // std::cout << heap.Delete() << std::endl
+    //           << std::endl;
 
-    std::cout << heap.Delete() << std::endl
-              << std::endl;
+    // std::cout << heap.Delete() << std::endl
+    //           << std::endl;
 
+    // std::cout << heap.Delete() << std::endl
+    //           << std::endl;
+
+    // std::cout << heap.Delete() << std::endl
+    //           << std::endl;
+
+    // std::cout << heap.Delete() << std::endl
+    //           << std::endl;
+    
+    // std::cout << heap.Delete() << std::endl
+    //           << std::endl;
+    
+    // std::cout << heap.Delete() << std::endl
+    //           << std::endl;
+    
     for (int data : heap.heapArr)
     {
         std::cout << data << std::endl;
